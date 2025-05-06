5e94a2ae2ba8feb6bee726754ab2110d7b607759
Pedro Henrique Gaspar <106278541+PedroGasparr@users.noreply.github.com>
2025-05-06 08:41:58 -0300

    1.1.8

---
+++ js.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvWJeddnSspl1qQbwX77wAcZnTH4Yacjc",
+    authDomain: "documentosgzl.firebaseapp.com",
+    projectId: "documentosgzl",
+    storageBucket: "documentosgzl.firebasestorage.app",
+    messagingSenderId: "254349635604",
+    appId: "1:254349635604:web:1809b9e6dfccb625453701",
+    measurementId: "G-LVCQXW7Q56"
+  };
